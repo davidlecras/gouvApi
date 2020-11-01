e92f1cd4789ea34a6e7e4220235c69cc5049f05d
David Lecras <davidlecras@hotmail.com>
2020-11-01 16:24:56 +0100

    generating home page

--- src/components/UI/Navbar/Navbar.js
+++ src/components/UI/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const navbar = (props) => {
     <>
       <Navbar bg="dark" variant="dark" expand="lg">
         <LinkContainer to="/">
-          <Navbar.Brand>API</Navbar.Brand>
+          <Navbar.Brand>IDF</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
--- src/containers/ApplicationWebsite/Home/Home.js
+++ src/containers/ApplicationWebsite/Home/Home.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Title from "../../../components/UI/Title/Title";
+import HomeImage from "../../../assets/images/chateau.jpg";
+import { Alert, Image, Container} from "react-bootstrap";
 
-class Home extends Component{
+class Home extends Component {
   render() {
-    return(
-      <div>Home</div>
-    )
+    return (
+      <Container fluid>
+        <Title>Bienvenue sur le site de la Région Île-de-France</Title>
+        <Alert variant="info" className="text-center">
+          Le site qui vous aide à trouver les établissements publics
+          d'Île-deFrance
+        </Alert>
+        <Image src={HomeImage} fluid style={{width:"100%"}} thumbnail/>
+      </Container>
+    );
   }
 }
 export default Home;
